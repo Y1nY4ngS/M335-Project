37f7c18708d881dd5749c036477cf44375be0016
Quoc Hung Nguyen <quochungnguyen1105@gmail.com>
2026-03-12 07:04:59 +0100

    0.1.0 Screen 1: usecase finished

---
+++ assets/data/items.js
@@ -0,0 +1,84 @@
+export const items = [
+    {
+        id: 1,
+        name: "Goldne Gazelle",
+        locationhint: [
+            "Layali Grove - Sparkling Empress Hotel",
+            "Space City - Core Area"
+        ],
+        maps: ["Layali Grove", "Space City"]
+    },
+    {
+        id: 2,
+        name: "Reinforced Carbon Fiberboard",
+        locationhint: [
+            "Zero Damm - Cement Plant",
+            "Space City - Industrial Zone"
+        ],
+        maps: ["Zero Damm", "Space City"]
+    },
+    {
+        id: 3,
+        name: "Rocket Fuel",
+        locationhint: [
+            "Space City - Industrial Zone",
+            "Space City - Core Area"
+        ],
+        maps: ["Space City"]
+    },
+    {
+        id: 4,
+        name: "Video Camera",
+        locationhint: [
+            "Layali Grove - Haavk WSR Lab",
+            "Space City - Core Area"
+        ],
+        maps: ["Layali Grove", "Space City"]
+    },
+    {
+        id: 5,
+        name: "Graphics Card",
+        locationhint: [
+            "Layali Grove - Haavk WSR Lab",
+            "Space City - Core Area"
+        ],
+        maps: ["Layali Grove", "Space City"]
+    },
+    {
+        id: 6,
+        name: "Military UAV",
+        locationhint: [
+            "Layali Grove - Haavk WSR Lab",
+            "Space City - Core Area"
+        ],
+        maps: ["Layali Grove", "Space City"]
+    },
+    {
+        id: 7,
+        name: "G.T.I Satcom Antenna",
+        locationhint: [
+            "Space City - Dormitory Area",
+            "Space City - Core Area"
+        ],
+        maps: ["Space City"]
+    },
+    {
+        id: 8,
+        name: "Experimental Data",
+        locationhint: [
+            "Zero Damm - Administrative Area",
+            "Space City - Core Area"
+        ],
+        maps: ["Zero Damm", "Space City"]
+    },
+    {
+        id: 9,
+        name: "Quantum Storage",
+        locationhint: [
+            "Zero Damm - Administrative Area",
+            "Space City - Core Area"
+        ],
+        maps: ["Zero Damml", "Space City"]
+    },
+
+]
